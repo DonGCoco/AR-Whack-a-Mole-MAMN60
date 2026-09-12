2415def7c15a6de82965f0a39f3218e2292ab078
DONG Jieru <91780000+DonGCoco@users.noreply.github.com>
2026-09-12 16:54:02 +0200

    Add AR plane placement controller

---
+++ Assets/Scripts/ARPlacementController.cs
@@ -0,0 +1,204 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementController : MonoBehaviour
+{
+    [SerializeField] private ARRaycastManager raycastManager;
+    [SerializeField] private ARPlaneManager planeManager;
+    [SerializeField] private Camera arCamera;
+
+    private static readonly List<ARRaycastHit> Hits = new List<ARRaycastHit>();
+
+    private GameObject reticle;
+    private WhackAMoleGame game;
+    private GUIStyle labelStyle;
+    private GUIStyle buttonStyle;
+
+    public void Configure(ARRaycastManager raycast, ARPlaneManager planes, Camera camera)
+    {
+        raycastManager = raycast;
+        planeManager = planes;
+        arCamera = camera;
+    }
+
+    private void Awake()
+    {
+        if (raycastManager == null)
+            raycastManager = GetComponent<ARRaycastManager>();
+
+        if (planeManager == null)
+            planeManager = GetComponent<ARPlaneManager>();
+
+        if (arCamera == null)
+            arCamera = Camera.main;
+
+        CreateReticle();
+    }
+
+    private void Update()
+    {
+        if (arCamera == null)
+            arCamera = Camera.main;
+
+        if (game == null)
+        {
+            UpdateReticle();
+
+            if (TryGetPressPosition(out Vector2 pressPosition))
+                TryPlaceGame(pressPosition);
+        }
+        else if (TryGetPressPosition(out Vector2 pressPosition))
+        {
+            game.TryHit(pressPosition, arCamera);
+        }
+    }
+
+    private bool TryGetPressPosition(out Vector2 position)
+    {
+        position = default;
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            position = Input.mousePosition;
+            return true;
+        }
+#endif
+
+        if (Input.touchCount == 0)
+            return false;
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began)
+            return false;
+
+        position = touch.position;
+        return true;
+    }
+
+    private void UpdateReticle()
+    {
+        if (raycastManager == null || reticle == null)
+            return;
+
+        Vector2 screenCenter = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+        bool foundPlane = raycastManager.Raycast(screenCenter, Hits, TrackableType.PlaneWithinPolygon);
+
+        reticle.SetActive(foundPlane);
+        if (!foundPlane)
+            return;
+
+        Pose pose = Hits[0].pose;
+        reticle.transform.SetPositionAndRotation(pose.position + pose.up * 0.002f, pose.rotation);
+    }
+
+    private void TryPlaceGame(Vector2 screenPosition)
+    {
+        if (raycastManager == null || arCamera == null)
+            return;
+
+        if (!raycastManager.Raycast(screenPosition, Hits, TrackableType.PlaneWithinPolygon))
+            return;
+
+        Pose hitPose = Hits[0].pose;
+
+        Vector3 forward = Vector3.ProjectOnPlane(arCamera.transform.forward, hitPose.up);
+        Quaternion rotation = hitPose.rotation;
+        if (forward.sqrMagnitude > 0.001f)
+            rotation = Quaternion.LookRotation(forward.normalized, hitPose.up);
+
+        GameObject gameObject = new GameObject("AR Whack-a-Mole Game");
+        gameObject.transform.SetPositionAndRotation(hitPose.position, rotation);
+
+        game = gameObject.AddComponent<WhackAMoleGame>();
+        game.Initialize();
+
+        if (reticle != null)
+            reticle.SetActive(false);
+
+        if (planeManager != null)
+        {
+            foreach (ARPlane plane in planeManager.trackables)
+                plane.gameObject.SetActive(false);
+
+            planeManager.enabled = false;
+        }
+    }
+
+    private void CreateReticle()
+    {
+        reticle = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        reticle.name = "Placement Reticle";
+        reticle.transform.localScale = new Vector3(0.05f, 0.001f, 0.05f);
+
+        Collider collider = reticle.GetComponent<Collider>();
+        if (collider != null)
+            Destroy(collider);
+
+        Renderer renderer = reticle.GetComponent<Renderer>();
+        Material material = new Material(Shader.Find("Standard"));
+        material.color = new Color(0.25f, 1f, 0.45f, 0.75f);
+        renderer.material = material;
+
+        reticle.SetActive(false);
+    }
+
+    public void ResetGame()
+    {
+        if (game != null)
+            Destroy(game.gameObject);
+
+        game = null;
+
+        if (planeManager != null)
+            planeManager.enabled = true;
+    }
+
+    private void EnsureStyles()
+    {
+        if (labelStyle == null)
+        {
+            labelStyle = new GUIStyle(GUI.skin.label)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = Mathf.Max(22, Screen.width / 22),
+                fontStyle = FontStyle.Bold,
+                normal = { textColor = Color.white }
+            };
+        }
+
+        if (buttonStyle == null)
+        {
+            buttonStyle = new GUIStyle(GUI.skin.button)
+            {
+                fontSize = Mathf.Max(18, Screen.width / 28)
+            };
+        }
+    }
+
+    private void OnGUI()
+    {
+        EnsureStyles();
+
+        float margin = Screen.width * 0.04f;
+        float topHeight = Screen.height * 0.11f;
+
+        string message = game == null
+            ? "Scan a table, then tap to place the game"
+            : $"Score: {game.Score}";
+
+        GUI.Label(new Rect(margin, margin, Screen.width - margin * 2f, topHeight), message, labelStyle);
+
+        if (game != null)
+        {
+            float buttonWidth = Mathf.Min(260f, Screen.width * 0.32f);
+            float buttonHeight = Mathf.Min(90f, Screen.height * 0.075f);
+            Rect buttonRect = new Rect(Screen.width - margin - buttonWidth, Screen.height - margin - buttonHeight, buttonWidth, buttonHeight);
+
+            if (GUI.Button(buttonRect, "Reset", buttonStyle))
+                ResetGame();
+        }
+    }
+}
